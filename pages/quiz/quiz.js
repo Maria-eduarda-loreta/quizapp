956576c7a532b7408bc228a33bf6fb15b26b8241
Maria Eduarda Loreta <eduardaloreta2206@gmail.com>
2024-06-20 03:47:49 +0000

    adicionadas as funções do botão de proxima pergunta e de finalizar o quiz

--- pages/quiz/quiz.js
+++ pages/quiz/quiz.js
@@ -110,18 +110,18 @@ function montarPergunta(){
 
             </form>
 
-            <button>Enviar</button>
+            <button>Responder</button>
 
         </section>
 
     `
 }
 
-function alterarSinais(texto){
+function alterarSinais(texto) {
     return texto.replace(/</g, "&lt;").replace(/>/g, "&gt;")
 }
 
-function guardarResposta(evento){
+function guardarResposta(evento) {
    resposta = evento.target.value
    idInputResposta = evento.target.id 
 
@@ -130,7 +130,20 @@ function guardarResposta(evento){
 
 }
 
-function validarResposta(){
+function validarResposta() {
+    const botaoEnviar = document.querySelector(".alternativas button")
+    botaoEnviar.removeEventListener("click", validarResposta)
+    botaoEnviar.addEventListener("click", proximaPergunta)
+
+    botaoEnviar.innerText = "Próxima"
+
+    if (pergunta === 10) {
+        botaoEnviar.innerText = "Finalizar"
+        botaoEnviar.addEventListener("click", finalizar)
+    } else {
+        botaoEnviar.addEventListener("click", proximaPergunta)
+    }
+
     if (resposta === quiz.questions[pergunta-1].answer){
         document.querySelector(`label[for='${idInputResposta}']`).setAttribute("id", "correta")
         pontos = pontos + 1
@@ -138,13 +151,22 @@ function validarResposta(){
         document.querySelector(`label[for='${idInputResposta}']`).setAttribute("id", "errada")
         document.querySelector(`label[for='${respostaCorretaId}']`).setAttribute("id", "correta")
     }
+
+    pergunta = pergunta + 1
 }
 
-async function iniciar(){
-    alterarAssunto()
-    await buscarPerguntas()
-    montarPergunta()
+function finalizar (){
+    localStorage.setItem("pontos", pontos)
 
+    window.location.href = "../resultado/resultado.html"
+}
+
+function proximaPergunta () {
+    montarPergunta()
+    adicionarEventoInputs()
+}
+
+function adicionarEventoInputs() {
     const inputsResposta = document.querySelectorAll(".alternativas input")
     inputsResposta.forEach(input => {
         input.addEventListener("click", guardarResposta)
@@ -153,7 +175,13 @@ async function iniciar(){
             respostaCorretaId = input.id
         }
     })
+}
 
+async function iniciar(){
+    alterarAssunto()
+    await buscarPerguntas()
+    montarPergunta()
+    adicionarEventoInputs()
 }
 
 iniciar()
